b91836791c715aa9f32185a462f8a72963171841
yousef alsayed abdelfattah abo elkhear <134104130+yousefattaff@users.noreply.github.com>
2023-10-01 23:33:21 +0300

    Update steppermotor_prv.h

--- HAL/StepperMotor/steppermotor_prv.h
+++ HAL/StepperMotor/steppermotor_prv.h
@@ -3,7 +3,7 @@
 #define HAL_STEPPERMOTOR_STEPPERMOTOR_PRV_H_
 
 
-// This is the number of coils in the stepper motor
-#define COIL_NUM 2048
+// This is the number of steps per revolution in the stepper motor
+#define COIL_NUM 2038
 
 #endif /* HAL_STEPPERMOTOR_STEPPERMOTOR_PRV_H_ */
